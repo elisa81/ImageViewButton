db1ba75d521b74c356bd5571cb347e35de0e981f
EDU <elisacho81@gmail.com>
2018-11-13 15:43:13 +0900

    view gallery and take picture button added

--- app/src/main/java/com/example/edu/imageviewbutton/MainActivity.java
+++ app/src/main/java/com/example/edu/imageviewbutton/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.edu.imageviewbutton;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,9 +15,11 @@ import android.widget.ImageView;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int LOAD_IMAGE = 101;
+    final int LOAD_IMAGE = 101;
+    final int IMAGE_CAPTURE = 102;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +27,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button fromGalleryButton = findViewById(R.id.fromGalleryButton);
+        Button imageCaptureButton = findViewById(R.id.imageCaptureButton);
+
         fromGalleryButton.setOnClickListener(this);
+        imageCaptureButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, LOAD_IMAGE);
-    }
 
-    protected void onActivityResult(int requestCode, int resultCOde, Intent data) {
+        switch (v.getId()) {
+            case R.id.fromGalleryButton:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, LOAD_IMAGE);
+                break;
 
-        ImageView imageViewFromGallery = findViewById(R.id.imageViewFromGallery);
+            case R.id.imageCaptureButton:
 
-        if (data != null) {
-            Uri selectedImage = data.getData();
-            InputStream inputStream = null;
-            try {
-                inputStream = this.getContentResolver().openInputStream(selectedImage);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-            imageViewFromGallery.setImageBitmap(bitmap);
+                if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+                    intent = new Intent();
+                    intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, IMAGE_CAPTURE);
+                }
+                break;
+
         }
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        ImageView imageViewFromGallery = findViewById(R.id.imageViewFromGallery);
+
+
+        switch (requestCode) {
+            case LOAD_IMAGE:
+                if (data != null) {
+                    Uri selectedImage = data.getData();
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = this.getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    imageViewFromGallery.setImageBitmap(bitmap);
+                }
+                break;
+            case IMAGE_CAPTURE:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap bitmap = (Bitmap) extras.get("data");
+                    imageViewFromGallery.setImageBitmap(bitmap);
+                }
+                break;
+        }
+    }
 }
